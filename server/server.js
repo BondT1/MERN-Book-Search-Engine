f775e315b7f25f2c34968f2a688923f5244a374c
BondT1 <tombond248@gmail.com>
2023-08-29 19:52:23 +0100

    implementing Apollo Server server.js

--- server/server.js
+++ server/server.js
@@ -1,18 +1,34 @@
+require("dotenv").config();
 const express = require('express');
 const path = require('path');
+
+const { ApolloServer } = require("apollo-server-express");
+const { authMiddleware } = require("./utils/auth");
+const { typeDefs, resolvers } = require("./schemas");
+
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+
 
 app.use(routes);
 
