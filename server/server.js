c1e45b09c3931abe31313a6d9937b12f4f41534f
BondT1 <tombond248@gmail.com>
2023-08-29 20:19:10 +0100

    further work on implementing Apollo API and applying to express as middleware

--- server/server.js
+++ server/server.js
@@ -28,10 +28,16 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-
-
 app.use(routes);
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"))
+})
+
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`GraphQL link hhtp://localhost:${PORT}${serever.graphqlPath}`);
+  });
 });
+
