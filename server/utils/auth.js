2a6730a91c846ae566767ad8de474300c5de844f
BondT1 <tombond248@gmail.com>
2023-08-29 19:26:33 +0100

    auth.js refactored to work with new API

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -12,11 +12,8 @@ module.exports = {
   },
 
   authMiddleware: function ({ req }) {
-
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
-
-    // ["Bearer", "<tokenvalue>"]
+    let token = req.body.token || req.query.token || req.headers.authorization;
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
@@ -33,13 +30,6 @@ module.exports = {
       console.log('Invalid token');
       return res.status(400).json({ message: 'invalid token!' });
     }
-
-    // send to next endpoint
-    next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
+
