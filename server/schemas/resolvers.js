41587f66325f62fad345104c5654bad2bce06e92
BondT1 <tombond248@gmail.com>
2023-08-29 21:07:46 +0100

    typeDefs.js schema created

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,9 @@
+// import user model
+const { User } = require('../models');
+// import sign token function from auth
+const { signToken } = require('../utils/auth');
+// import authentication error
+const { AuthenticationError } = require("apollo-server-express");
+
+
+
