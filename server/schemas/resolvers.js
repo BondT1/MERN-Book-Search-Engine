00431e7b4052eba01507fbb8e1e7ac6f462afc90
BondT1 <tombond248@gmail.com>
2023-08-31 19:19:38 +0100

    add user mutation resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -23,5 +23,14 @@ const resolvers = {
                 .populate("book");
         },
     },
+
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user }
+        }
+    }
 }
 
