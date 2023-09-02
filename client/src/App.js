98d316b3521905ab584012aab1e12c8ac945c691
BondT1 <tombond248@gmail.com>
2023-09-02 20:15:48 +0100

    started creating apolloclient and importing in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createdHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context'
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+
+const httpLink = createdHttpLink({
+  url: '/graphql',
+});
 
 function App() {
   return (
