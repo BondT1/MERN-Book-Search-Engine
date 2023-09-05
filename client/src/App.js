be9301a15f931a3627f64f3d9d7f0a9e369431ec
BondT1 <tombond248@gmail.com>
2023-09-05 14:31:18 +0100

    endless bug fixing

--- client/src/App.js
+++ client/src/App.js
@@ -34,7 +34,6 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-        <>
           <Navbar />
           <Routes>
             <Route 
@@ -50,7 +49,6 @@ function App() {
               element={<h1 className='display-2'>Wrong page!</h1>}
             />
           </Routes>
-        </>
       </Router>
     </ApolloProvider>
   );
